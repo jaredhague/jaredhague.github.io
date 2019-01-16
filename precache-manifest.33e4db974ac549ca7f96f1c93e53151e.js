5f7228b33a289e62ad85eb1f0f22c9209b6ae1a9
jaredhague <jared@postfilmdesign.com>
2019-01-15 20:42:08 -0500

    Adding static files

---
+++ precache-manifest.33e4db974ac549ca7f96f1c93e53151e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f18580d9c147a627252",
+    "url": "/static/js/main.0f18580d.chunk.js"
+  },
+  {
+    "revision": "66b56df8329a7742ca8f",
+    "url": "/static/js/1.66b56df8.chunk.js"
+  },
+  {
+    "revision": "0f18580d9c147a627252",
+    "url": "/static/css/main.3aeff7ca.chunk.css"
+  },
+  {
+    "revision": "93f427e82c45349a9124d585ab9eea82",
+    "url": "/index.html"
+  }
+];
